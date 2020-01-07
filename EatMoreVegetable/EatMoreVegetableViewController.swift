188267d27f68921876c29c36a1156a2d1b10ba37
Bugrahan <donmezbugrahan@gmail.com>
2020-01-07 14:20:46 +0300

    Add placeholder for empty table view

--- EatMoreVegetable/EatMoreVegetableViewController.swift
+++ EatMoreVegetable/EatMoreVegetableViewController.swift
@@ -61,6 +61,17 @@ class EatMoreVegetableViewController: UIViewController, UITableViewDataSource, U
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if (foodItems.count == 0) {
+            
+            tableView.setEmptyView(title: "You don't have any history.", message: "Your meals will be in here.")
+        }
+        
+        else {
+            
+            tableView.restore()
+        }
+        
         return foodItems.count
     }
     
@@ -208,4 +219,47 @@ extension EatMoreVegetableViewController {
     }
     
 
+}
+
+//MARK: -UITableView
+extension UITableView {
+    
+    func setEmptyView(title: String, message: String) {
+        
+        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+        let titleLabel = UILabel()
+        let messageLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        titleLabel.textColor = UIColor.lightGray
+        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+        
+        messageLabel.textColor = UIColor.darkGray
+        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+        
+        emptyView.addSubview(titleLabel)
+        emptyView.addSubview(messageLabel)
+        
+        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+        
+        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+        
+        titleLabel.text = title
+        messageLabel.text = message
+        messageLabel.numberOfLines = 0
+        messageLabel.textAlignment = .center
+        
+        self.backgroundView = emptyView
+        self.separatorStyle = .none
+    }
+    
+    func restore() {
+        
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
 }
