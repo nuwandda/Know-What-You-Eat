5cdfa76776582676858c84565ff0e0e376b38f67
Bugrahan Donmez <bugra@rapsodo.com>
2020-01-08 09:42:13 +0300

    Add SignUpViewController

---
+++ EatMoreVegetable/SignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SignUpViewController.swift
+//  EatMoreVegetable
+//
+//  Created by Rapsodo Mobile 6 on 8.01.2020.
+//  Copyright © 2020 Rapsodo Mobile 6. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
