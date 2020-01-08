f0f4c64c7085f3e1ed30ea91593f2e0d83400c6e
Bugrahan Donmez <bugra@rapsodo.com>
2020-01-08 11:04:03 +0300

    Update StartViewController

--- EatMoreVegetable/StartViewController.swift
+++ EatMoreVegetable/StartViewController.swift
@@ -16,13 +16,16 @@ class StartViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
+    }
+    
+    fileprivate func setupUI() {
         loginButton.layer.borderWidth = 1
         loginButton.layer.borderColor = UIColor.white.cgColor
         
         signupButton.layer.borderWidth = 1
         signupButton.layer.borderColor = UIColor.white.cgColor
     }
-    
     
     @IBAction func loginTapped(_ sender: UIButton) {
     }
